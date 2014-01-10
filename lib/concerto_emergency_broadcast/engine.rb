8b7a5eed7366fc57caea019ac311415ec86f2276
Gabriel <perezg2@rpi.edu>
2014-01-10 01:27:53 -0500

    Initial work on template override

--- lib/concerto_emergency_broadcast/engine.rb
+++ lib/concerto_emergency_broadcast/engine.rb
@@ -17,10 +17,39 @@ module ConcertoEmergencyBroadcast
           Rails.logger.info "ConcertoEmergencyBroadcast: Initialization code is running"
         end
 
+        # Controller hook and view hook for Screen Show page
+        # the app crashed when I didn't have these added
         add_controller_hook "ScreensController", :show, :before do
         end
-
         add_view_hook "ScreensController", :screen_details, :text => ""
+
+
+        # Controller hook for frontend/screens_controller#show
+        add_controller_hook "Frontend::ScreensController", :setup, :before do
+          # Check for active EmergencyAlert Content
+          emergency_contents = Submission.joins(:content).where(:submissions => {:moderation_flag => true}, :contents => {:type => 'EmergencyAlert'})
+          
+          # EmergencyAlert content is present, override the screen
+          if not emergency_contents.empty?
+            # Find our emergenct template
+            emergency_template = Template.find_by_name('Emergency Template')
+
+            # emergency template not found in database
+            if emergency_template.id.nil?
+              # create a new one?
+            end
+
+            # Since an emergency alert was detected, make a copy of the 
+            # requested screen and modify the setup to display the alerts
+            emergency_screen = @screen
+            emergency_screen.template_id = emergency_template.id
+
+
+            # Override the screen variable that the 
+            # Frontend::ScreensController will respond with 
+            @screen = emergency_screen
+          end
+        end
 
       end
     end
